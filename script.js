25fa8323add96d6ce51f0a8dcb82bac4c0ebecae
Adriana Villegas <villegas.a102@gmail.com>
2022-07-22 00:11:02 -0500

    completed project

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var cityWeatherButton = document.getElementById('searchButton');
-var textBox = document.getElementById('citySearch');
+// var textBox = document.getElementById('citySearch');
 var cityList = document.getElementById('cities');
 
-function getApi() {
- var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=c6a435067a34d7baa1c0f2557358b1ff&units=imperial';
+cityWeatherButton.addEventListener('click', function() {
+    textInfo = document.getElementById('citySearch').value
+    requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${textInfo}&appid=c6a435067a34d7baa1c0f2557358b1ff&units=imperial`;
 
  fetch(requestURL)
   .then(function (response) {
@@ -15,12 +16,34 @@ function getApi() {
     console.log(weatherData.main.temp)
     console.log(weatherData.wind.speed)
     console.log(weatherData.main.humidity)
+
+    // Pulling our specific data for each detail needed
+    var temp = weatherData.main.temp + '\u00b0 F'
+    var wind = weatherData.wind.speed + ' MPH'
+    var humid = weatherData.main.humidity + '%'
+
+    
+    var tempInfo = document.querySelector('#degrees')
+    var windInfo = document.querySelector('#miles')
+    var humidInfo = document.querySelector('#percent')
+
+    // Displaying the information in the browser 
+    tempInfo.innerHTML = temp
+    windInfo.innerHTML = wind
+    humidInfo.innerHTML = humid
+
     for (var i=0; i < weatherData; i++) {
       var savedCities = document.createElement(li);
       savedCities.textContent = weatherData[i].name;
       cityList.appendChild(savedCities);   
     }
   });
-}
+})
 
-cityWeatherButton.addEventListener('click', getApi);
+// function newCity() {
+//   textBox.value
+//   var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${textBox.value}&appid=c6a435067a34d7baa1c0f2557358b1ff&units=imperial`
+//   return requestURL  
+// }
+
+// cityWeatherButton.addEventListener('click', getApi);
